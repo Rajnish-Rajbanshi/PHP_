00c75f5dcfc72c027c23b67e657bed1eb25159f0
Rajnish Rajbanshi <myreco100@gmail.com>
2026-06-11 23:19:16 +0545

    add...

--- Print_Series2.php
+++ Print_Series2.php
@@ -3,19 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WAP to print following series: 5, 10, 15, 20, ...... 50</title>
+    <title>WAP to print following series: 1, 4, 9, 16, ..... upto 20 terms.</title>
 </head>
 <body>
     <form action="" method="post">
-        <label for="print">Print series: 5, 10, 15, 20, ...... 50</label><br><br>
+        <label for="print">Print series: 1, 4, 9, 16, ..... upto 20 terms.</label><br><br>
         <input type="submit" name="print" value="Print Series"><br><br>
     </form>
 </body>
 </html>
 <?php
     if(isset($_POST['print'])){
-        for($i=5; $i<=50; $i+=5){
-            echo"<h3>$i</h3>";
+        for($i=1; $i<=20; $i++){
+            echo"<h3>".($i*$i)."</h3>";
         }
     }
 ?>
--- Print_Series3.php
+++ Print_Series3.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WAP to print following series: 5, 10, 15, 20, ...... 50</title>
+    <title>WAP to print following series: 100, 98, 96, 94, ............. Upto 10 terms.</title>
 </head>
 <body>
     <form action="" method="post">
-        <label for="print">Print series: 5, 10, 15, 20, ...... 50</label><br><br>
+        <label for="print">Print series: 100, 98, 96, 94, ............. Upto 10 terms.</label><br><br>
         <input type="submit" name="print" value="Print Series"><br><br>
     </form>
 </body>
 </html>
 <?php
     if(isset($_POST['print'])){
-        for($i=5; $i<=50; $i+=5){
+        for($i=100; $i>=10; $i-=2){
             echo"<h3>$i</h3>";
         }
     }
